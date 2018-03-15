10bd7149f2a77bd7591c8a128eb5216bb74afa19
haigbedros <haigbedros@gmail.com>
2018-03-14 17:14:22 -0700

    observer on the auth object

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,5 +40,13 @@
                 console.log(err);
             });   
     });
+
+    firebase.auth().onAuthStateChanged(function(user){
+        if (user) {
+            console.log(user);
+        } else {
+            console.log("no user!");
+        }
+    });
     
   });
